d5b347f82ffbd3dcf4a9cb858be6669d3a606421
Andrey Pudov <mail@andreypudov.com>
2023-03-03 13:23:31 +0100

    Exercise 1.3

---
+++ chapter-1/exercise-1-3.clj
@@ -0,0 +1,14 @@
+; Exercise 1.3: Define a procedure that takes three numbers
+; as arguments and returns the sum of the squares of the two
+; larger numbers.
+
+(ns sicp.solutions.chapter-1
+  (:require [clojure.math :as math]))
+
+(defn sum-squares-two-largest [a b c]
+  (- (+ (math/pow a 2)
+        (math/pow b 2)
+        (math/pow c 2))
+     (math/pow (min a b c) 2)))
+
+(println (sum-squares-two-largest 1 2 3))
