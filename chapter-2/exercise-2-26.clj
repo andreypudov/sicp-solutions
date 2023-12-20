5ec6d27047658e61b4e3ecb56558440892cbc8d0
Andrey Pudov <mail@andreypudov.com>
2023-12-20 20:19:02 +0100

    Exercise 2.26

---
+++ chapter-2/exercise-2-26.clj
@@ -0,0 +1,20 @@
+; Exercise 2.26: Suppose we define x and y to be two lists:
+;
+; (def x (list 1 2 3))
+; (def y (list 4 5 6))
+;
+; What result is printed by the interpreter in response to evaluating each of
+; the following expressions:
+;
+; (append x y)
+; (cons x y)
+; (list x y)
+
+(def append concat)
+
+(def x (list 1 2 3))
+(def y (list 4 5 6))
+
+(println (append x y))                  ; (1 2 3 4 5 6)
+(println (cons x y))                    ; ((1 2 3) 4 5 6)
+(println (list x y))                    ; ((1 2 3) (4 5 6))
