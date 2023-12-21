5e01a5b3d934b914c4bf9decf01ca6a7ea9481d6
Andrey Pudov <mail@andreypudov.com>
2023-12-21 17:31:12 +0100

    Exercise 2.28

--- chapter-2/exercise-2-28.clj
+++ chapter-2/exercise-2-28.clj
@@ -2,10 +2,21 @@
 ; (represented as a list) and returns a list whose elements are all the leaves
 ; of the tree arranged in left-to-right order. For example,
 ;
-; (define x (list (list 1 2) (list 3 4)))
+; (def x (list (list 1 2) (list 3 4)))
 ; (fringe x)
 ; (1 2 3 4)
 ; (fringe (list x x))
 ; (1 2 3 4 1 2 3 4)
 
 (ns sicp.solutions.chapter-2)
+
+(defn fringe [coll]
+  (cond
+    (empty? coll) coll
+    (not (coll? (first coll))) coll
+    :else (concat (fringe (first coll))
+                  (fringe (rest coll)))))
+
+(def x (list (list 1 2) (list 3 4)))
+(println (fringe x))                    ; (1 2 3 4)
+(println (fringe (list x x)))           ; (1 2 3 4 1 2 3 4)
